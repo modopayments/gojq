8da15f814942e3b96769b51a0f99e62c92887f54
Bion Oren <bion@modopayments.com>
2024-07-01 15:54:18 -0500

    new fork that just remove number normalization for RunWithContext for performance reasons (2.5x speedup observed)

--- compiler.go
+++ compiler.go
@@ -46,10 +46,7 @@ func (c *Code) RunWithContext(ctx context.Context, v any, values ...any) Iter {
 	} else if len(values) < len(c.variables) {
 		return NewIter(&expectedVariableError{c.variables[len(values)]})
 	}
-	for i, v := range values {
-		values[i] = normalizeNumbers(v)
-	}
-	return newEnv(ctx).execute(c, normalizeNumbers(v), values...)
+	return newEnv(ctx).execute(c, v, values...)
 }
 
 type scopeinfo struct {
