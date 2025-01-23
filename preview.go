dfb3a10431d6e9c4e5fa38a66f65cbbaaa88cc26
Bion Oren <bion@modopayments.com>
2025-01-23 12:18:52 -0600

    extreme truncation in JQ value previews for security/compliance

--- preview.go
+++ preview.go
@@ -1,18 +1,19 @@
 package gojq
 
-import "unicode/utf8"
-
 // Preview returns the preview string of v. The preview string is basically the
 // same as the jq-flavored JSON encoding returned by [Marshal], but is truncated
-// by 30 bytes, and more efficient than truncating the result of [Marshal].
+// by 1 byte, and more efficient than truncating the result of [Marshal].
 //
 // This method is used by error messages of built-in operators and functions,
 // and accepts only limited types (nil, bool, int, float64, *big.Int, string,
 // []any, and map[string]any). Note that the maximum width and trailing strings
 // on truncation may be changed in the future.
+//
+// This function was changed to return a maximum of 1 character, to avoid leaking PCI/PII data in error messages.
+// Extreme truncation is preferred to masking, because truncation can leak partial sensitive data that is too short to identify for masking, but too long for compliance.
 func Preview(v any) string {
-	bs := jsonLimitedMarshal(v, 32)
-	if l := 30; len(bs) > l {
+	bs := jsonLimitedMarshal(v, 2)
+	if l := 1; len(bs) > l {
 		var trailing string
 		switch v.(type) {
 		case string:
@@ -23,10 +24,6 @@ func Preview(v any) string {
 			trailing = " ...}"
 		default:
 			trailing = " ..."
-		}
-		for len(bs) > l-len(trailing) {
-			_, size := utf8.DecodeLastRune(bs)
-			bs = bs[:len(bs)-size]
 		}
 		bs = append(bs, trailing...)
 	}
